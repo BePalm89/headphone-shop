673b236880414db1449ee2d05ee221cc2e873f86
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2023-11-13 19:30:46 +0100

    clear code

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ import { products } from './components/Products/Products';
 import { filtersTemplateAndEventListeners } from './components/Filters/Filters';
 import { PRODUCTS } from './constants';
 
-const application = () => {
+const loadApplication = () => {
     navbar();
     products(PRODUCTS);
     filtersTemplateAndEventListeners();
     footer();
 }
 
-application();
+loadApplication();
 
     
